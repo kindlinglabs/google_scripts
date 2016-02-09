d23a3ac51b73d96a1fed3b4f62a78c0a294bb372
JP Slavinsky <jpslav@gmail.com>
2016-02-08 22:53:59 -0800

    fixes to archiving old calendar invites

--- archive_old_calendar_invites.js
+++ archive_old_calendar_invites.js
@@ -8,20 +8,39 @@ function archiveOldCalendarInvites() {
   for (var i = 0; i < threads.length; i++) {
     var thread = threads[i];
     var subject = thread.getMessages()[0].getSubject();
-    var matches = /.* @ (.+)( \S+ - \S+)* \(/.exec(subject);
 
-    if (matches == null || matches.length < 2) {
+    // The \s.? is a bit of a mystery to me; appears that some subjects have extra whitespace.
+    var matches = /\D{3} \D{3} \d+,\s.?\d{4}/.exec(subject);
+
+    if (matches == null || matches.length < 1) {
       continue;
     }
 
-    var subjectDateString = matches[1];
+    var subjectDateString = matches[0];
     var subjectDate = new Date(subjectDateString);
 
-    if (subjectDate < cutoffDate) {
+    if (dateIsValid(subjectDate) && subjectDate < cutoffDate) {
       oldThreads.push(thread);
     }
   }
 
   GmailApp.moveThreadsToArchive(oldThreads);
 
+}
+
+function dateIsValid(d) {
+  // http://stackoverflow.com/a/1353711
+
+  if ( Object.prototype.toString.call(d) === "[object Date]" ) {
+    // it is a date
+    if ( isNaN( d.getTime() ) ) {  // d.valueOf() could also work
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  else {
+    return false;
+  }
 }
